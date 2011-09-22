1abeec64764a3b07f28216ac456090126b8877de
hideki.shima@gmail.com <hideki.shima@gmail.com@e8eec38a-6233-703b-c7dd-03771fa02b89>
2011-09-22 10:48:41 +0000

    cli added; unzip updated

--- src/main/java/com/gmail/hidekishima/helloworld/zip/Unzip.java
+++ src/main/java/com/gmail/hidekishima/helloworld/zip/Unzip.java
@@ -3,36 +3,88 @@ package com.gmail.hidekishima.helloworld.zip;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
+/**
+ * Unzipping a zip file
+ * 
+ */
 public class Unzip {
 
   /**
-   * Unzip the specified file and return extracted dir
-   * @param zipPath
-   * @return
-   */
-  public static File unzip(String zipPath) throws FileNotFoundException {
-    FileInputStream is = new FileInputStream(zipPath);
-    return unzip(is);
-  }
-  
-  /**
-   * Unzip xmi-zip file and store into a temp dir.
+   * Recursively unzip xmi-zip file and store into a temp dir.
+   * 
+   * Modified based on a code at:
+   * http://stackoverflow.com/questions/981578/how-to-unzip-files-recursively-in-java
+   * 
    * @param is xmi zip input stream
-   * @return unzipped dir path
+   * @param unzipBase where to expand unzipped files 
    */
-  public static File unzip(InputStream is) {
-    String date = new SimpleDateFormat("yyyyMMdd-HHmmss").format(Calendar.getInstance().getTime());
-    File unzipBase = new File(System.getProperty("java.io.tmpdir")+"/oaqa-" + date);
+  public static void unzip(String zipFile, File unzipBase) throws ZipException, IOException {
     
+    int BUFFER = 2048;
+    File file = new File(zipFile);
+
+    ZipFile zip = new ZipFile(file);
+
+    Enumeration<?> zipFileEntries = zip.entries();
+
+    // Process each entry
+    while (zipFileEntries.hasMoreElements()) {
+      // grab a zip file entry
+      ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
+      String currentEntry = entry.getName();
+      File destFile = new File(unzipBase, currentEntry);
+      // destFile = new File(newPath, destFile.getName());
+      File destinationParent = destFile.getParentFile();
+
+      // create the parent directory structure if needed
+      destinationParent.mkdirs();
+
+      if (!entry.isDirectory()) {
+        BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
+        int currentByte;
+        // establish buffer for writing file
+        byte data[] = new byte[BUFFER];
+
+        // write the current file to disk
+        FileOutputStream fos = new FileOutputStream(destFile);
+        BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER);
+
+        // read and write until last byte is encountered
+        while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
+          dest.write(data, 0, currentByte);
+        }
+        dest.flush();
+        dest.close();
+        is.close();
+      }
+
+      if (currentEntry.endsWith(".zip")) {
+        // found a zip file, try to open
+        unzip(destFile.getAbsolutePath(), unzipBase);
+      }
+    }
+  }
+
+  @Deprecated
+  /**
+   * Incomplete non-recursive version.
+   * 
+   * This implementation can take a zip file as input stream
+   * and might be useful for receiving uploaded zip file...? 
+   * 
+   * @param is
+   * @param unzipBase
+   */
+  public static void unzipOld(InputStream is, File unzipBase) {
     BufferedOutputStream bos = null;
     BufferedInputStream bis = null;
     ZipInputStream in = null;
@@ -54,7 +106,7 @@ public class Unzip {
         }
         
         in.closeEntry();
-       
+        
         bos.close();
         bos = null;
       }
@@ -67,8 +119,6 @@ public class Unzip {
         bos.close();
       } catch (Exception e2) {}
     }
-    
-    return unzipBase;
   }
  
 }
